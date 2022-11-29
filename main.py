8fd51e79f8d66e699246ae8dd11963c4575c29be
Sérgio Oliveira <ade9@live.com.pt>
2022-11-29 14:39:15 +0000

    Created voice and overlay alarms

--- main.py
+++ main.py
@@ -1,38 +1,77 @@
 import os
 import threading
-
-# import vlc
 from seleniumwire.utils import decode
 from selenium.webdriver.chrome.options import Options
 from seleniumwire import webdriver
 import time
 import json
-from IPython.display import Audio
+from pygame import mixer  # Load the popular external library
+from time import sleep
 
-alarmTimeout = 20
-
+# Variables
+alarmsActive = True
 vsfPercentageLimit = 0.0
-lastAlarmTime = time.time() - alarmTimeout
 
-def notify(title, text):
+# Sound alarm
+soundAlarmTimeSeconds = 20
+lastSoundAlarmTime = time.time() - soundAlarmTimeSeconds
+
+# Voice alarm
+voiceAlarmTimeSeconds = 5
+lastVoiceAlarmTime = time.time() - voiceAlarmTimeSeconds
+
+# Overlay alarm
+overlayAlarmTimeSeconds = "5"
+lastOverlayAlarmTime = time.time() - int(overlayAlarmTimeSeconds)
+
+# Initialization
+thisFolder = os.path.dirname(os.path.abspath(__file__))
+mixer.init()
+mixer.music.set_volume(0.5)
+mixer.music.load(thisFolder + '/nuclear_alarm.mp3')
+
+def sayTTSLinuxIfNecessary(text):
+    if (time.time() - lastVoiceAlarmTime) > voiceAlarmTimeSeconds and alarmsActive:
+        os.system("espeak -v en-us -s 150 '" + text + "'")
+
+def overlayTextLinux(text):
+    if (time.time() - lastOverlayAlarmTime) > int(overlayAlarmTimeSeconds) and alarmsActive:
+        os.system("echo '" + text + "' | osd_cat -A center -p middle -f -*-fixed-medium-r-*-*-20-*-*-*-*-*-*-* -c green -d " + overlayAlarmTimeSeconds)
+
+def playSoundAlarm():
+    if (time.time() - lastSoundAlarmTime) > soundAlarmTimeSeconds and alarmsActive:
+        mixer.music.play()
+        sleep(soundAlarmTimeSeconds)
+
+def notify(text):
     global lastAlarmTime
-    if (time.time() - lastAlarmTime) > alarmTimeout:
-        os.system("""
-                  osascript -e 'display notification "{}" with title "{}"'
-                  """.format(text, title))
-        Audio(url="https://www.tones7.com/media/nuclear_alarm.mp3")
-        lastAlarmTime = time.time()
+    # threads for 3 alarms
+    t1 = threading.Thread(target=playSoundAlarm)
+    t2 = threading.Thread(target=sayTTSLinuxIfNecessary, args=(text,))
+    t3 = threading.Thread(target=overlayTextLinux, args=(text,))
+    t1.start()
+    t2.start()
+    t3.start()
 
 def changeVsfPercentageLimitInput():
     global vsfPercentageLimit
-    vsfPercentageLimit = float(input("Enter new vsfPercentageLimit: "))
+    global alarmsActive
+    userInput = input("Enter new vsfPercentageLimit or (start) (stop): ")
+    if userInput == "start":
+        alarmsActive = True
+        print("Alarms active")
+    if userInput == "stop":
+        alarmsActive = False
+        print("Alarms stopped")
+    else:
+        vsfPercentageLimit = float(userInput)
 
 def changeVsfPercentageLimit():
     while True:
         try:
             changeVsfPercentageLimitInput()
         except ValueError:
-            print("Please enter a number")
+            print("Not a number")
             continue
 
 options = webdriver.ChromeOptions()
@@ -62,9 +101,10 @@ def main():
                 if len(obj["data"]) == 1:
 
                     vsfPercentage = float(obj['data']['total'][0]['videoStartFailuresPercentage'])
+                    vsfPercentageTwoDecimalPlaces = round(vsfPercentage, 2)
 
                     if vsfPercentage > vsfPercentageLimit:
-                        notify("VsfPercentageLimit", "VsfPercentageLimit has been reached: " + str(vsfPercentage))
+                        notify("VsfPercentageLimit has been reached: " + str(vsfPercentageTwoDecimalPlaces))
                         break
 
         # Clear requests
